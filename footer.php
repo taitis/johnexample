c929d28a5e31870d4141fb3a8d5d2b9fe855ac4c
taitis <47973109+taitis@users.noreply.github.com>
2019-04-07 22:12:50 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+footer {
+background-color:#777;
+padding:10px;
+text-align: center;
+color:white;
+}
+</style>
+</head>
+<body></body>
+</html>
---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+header {
+background-color:#666;
+padding: 30 px;
+text-align: center;
+font-size:35px;
+color:white;
+}
+</style>
+</head>
+<body></body>
+</html>
